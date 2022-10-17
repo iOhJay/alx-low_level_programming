222cbf19d471b1f8ebea0c8b8591fdfcb9c34119
iOhJay <ohjaywunderkind@gmail.com>
2022-10-17 14:14:07 -0700

    a program that prints all possible different smallest combinations of two digits in ascending order, where numbers must be separated by ,, followed by a space, and two digits must be different, using the putchar function only

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all possible smallest combinations of two different digits,
+ * in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int Digit1, Digit2;
+
+	for (Digit1 = 0; Digit1 < 9; Digit1)
+	{
+		for (Digit2 = Digit1 + 1; Digit2 < 10; Digit2++)
+		{
+		putchar((Digit1 % 10) + '0');
+		putchar((Digit2 % 10) + '0');
+
+		if (Digit2 == 8 && Digit2 == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+		}
+	}
+		putchar('\n');
+
+		return (0);
+
+}
